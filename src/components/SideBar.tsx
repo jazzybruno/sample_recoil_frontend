641d2843252468252caa24a13214c2eb209abcb4
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2023-09-28 08:35:19 +0200

    Added the sidebar in the application

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,30 +1,45 @@
 import {BiSolidDashboard} from 'react-icons/bi'
+import {IoMdDoneAll} from 'react-icons/io'
+import {FaTasks} from 'react-icons/fa'
+import {AiOutlineDelete} from 'react-icons/ai'
+import {CgProfile} from 'react-icons/cg'
+import {IoLogOutOutline} from 'react-icons/io5'
 
 const SideBar = () => {
     return ( 
-     <div className="w-[100%] flex items-center flex-col">
+     <div className="w-[100%] flex items-center flex-col mt-2 ">
          <h2 className="text-2xl h-[10%] text-[#1ec08a] font-poppins mt-3 text-center font-bold">Tasks Manager</h2>
          <div className="flex justify-center items-center flex-col w-[68%]">
             {/* All links to the pages of the app */}
-            <div className="flex w-[100%] text-md flex-row justify-start items-center space-x-2 text-[1em] mt-10 text-black font-mooli">
-                <p><BiSolidDashboard/></p> 
+            <div className="flex w-[100%] text-md flex-row cursor-pointer justify-start items-center space-x-2 text-[1em] py-4 px-2 mt-5 text-black font-mooli hover:bg-green-300 hover:rounded-lg ">
+                <p className='text-md'><BiSolidDashboard/></p> 
                 <p className='text-black'>DashBoard</p>
                 </div>
 
-                <div className="flex w-[100%] text-md flex-col justify-start items-start mt-10 text-black font-mooli">
-                All Tasks
+                <div className="flex w-[100%] text-md flex-row cursor-pointer justify-start items-center space-x-2 text-[1em] py-4 px-2 mt-5 text-black font-mooli hover:bg-green-300 hover:rounded-lg">
+                <p className='text-md'><IoMdDoneAll /></p>
+                <p className='text-black'>Completed</p>
                 </div>
 
-                <div className="flex w-[100%] text-md flex-col justify-start items-start mt-10 text-black font-mooli">
-                Completed
+                <div className="flex w-[100%] text-md flex-row cursor-pointer justify-start items-center space-x-2 text-[1em] py-4 px-2 mt-5 text-black font-mooli hover:bg-green-300 hover:rounded-lg">
+                <p className='text-md'><AiOutlineDelete /></p>
+                <p className='text-black'>Deleted</p>
                 </div>
 
-                <div className="flex w-[100%] text-md flex-col justify-start items-start mt-10 text-black font-mooli">
-                Deleted
+                <div className="flex w-[100%] text-md flex-row cursor-pointer justify-start items-center space-x-2 text-[1em] py-4 px-2 mt-5 text-black font-mooli hover:bg-green-300 hover:rounded-lg">
+               <p className='text-md'><CgProfile /></p>
+               <p className='text-black'>Profile</p>
                 </div>
 
-                <div className="flex w-[100%] text-md flex-col justify-start items-start mt-10 text-black font-mooli">
-                Profile
+                 <div className="flex w-[100%] text-md flex-row cursor-pointer justify-start items-center space-x-2 text-[1em] py-4 px-2 mt-5 text-black font-mooli hover:bg-green-300 hover:rounded-lg">
+                <p className='text-md'><FaTasks /></p>
+                <p className='text-black'>Tasks</p>
+                </div>
+
+
+                <div className="flex w-[100%] text-md flex-row cursor-pointer justify-start items-center space-x-2 text-[1em] py-4 px-2 mt-5 text-black font-mooli hover:bg-green-300 hover:rounded-lg">
+                <p className='text-md'><IoLogOutOutline /></p>
+                <p className='text-black'>Logout</p>
                 </div>
          </div>
      </div> 
--- src/pages/DashBaord.tsx
+++ src/pages/DashBaord.tsx
@@ -3,11 +3,11 @@ import SideBar from "../components/SideBar";
 const DashBoard = () => {
     return ( 
         <div className="bg-white grid h-[100%] w-[100%]">
-            <div className="flex justify-center items-start w-[20%] ">
+            <div className="flex justify-center items-start w-[18%] bg-gray-100 h-[100vh] ">
                 {/* Side Bar Links /\ */}
                 <SideBar />
             </div>
-            <div className="flex justify-center items-start w-[80%]">
+            <div className="flex justify-center items-start w-[82%]">
                 {/* Content of the Dashboard / */}
             </div>
         </div>
